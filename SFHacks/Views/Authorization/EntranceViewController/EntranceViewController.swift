a32faaae2e18b48f7b582723061c8e0e8ede5cb4
Kam Khai <patrick.phyo@gmail.com>
2019-03-02 22:05:53 -0800

    finish the loginView

--- SFHacks/Views/Authorization/EntranceViewController/EntranceViewController.swift
+++ SFHacks/Views/Authorization/EntranceViewController/EntranceViewController.swift
@@ -7,24 +7,105 @@
 //
 
 import UIKit
+import SVProgressHUD
+
 
 class EntranceViewController: UIViewController {
-
+    
+    private var controller = AuthenticationController()
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        hideKeyboardWhenTappedAround()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //start of signIn
+    
+    private func signIn() {
+        guard let email = emailTextField.text else { return }
+        guard let password = passwordTextField.text else { return }
+        
+        guard controller.validateEmail(email) else {
+            p("invalid email")
+            showAlertWithoutCancel(title: "Invalid e-mail format.", description: "")
+            return
+        }
+        let validationResult = controller.validatePassword(password)
+        guard validationResult.succeeded else {
+            p("invalid password")
+            showAlertWithoutCancel(title: validationResult.error, description: "")
+            return
+        }
+        
+        SVProgressHUD.show()
+        controller.signInWithEmail(
+            email: email,
+            password: password,
+            success: {
+                SVProgressHUD.dismiss()
+                ScreenTransitionManager.moveToMapBrowserViewController()
+        },
+            failure: { [weak self] in
+                SVProgressHUD.dismiss()
+                showAlertWithoutCancel(title: "ログインに失敗しました。", description: "")
+                //                AlertManager.showConnectionErrorAlertCancellable(retryTask: { [weak self] in
+                //                    guard let sself = self else { return }
+                //                    sself.signIn()
+                //                })
+        })
     }
-    */
-
+    
+    
+    //start of Signup
+    private func signUp() {
+        guard let email = emailTextField.text else { return }
+        guard let password = passwordTextField.text else { return }
+        
+        guard controller.validateEmail(email) else {
+            p("invalid email")
+            showAlertWithoutCancel(title: "Invalid e-mail format.", description: "")
+            return
+        }
+        let validationResult = controller.validatePassword(password)
+        guard validationResult.succeeded else {
+            p("invalid password")
+            showAlertWithoutCancel(title: validationResult.error, description: "")
+            return
+        }
+        
+        SVProgressHUD.show()
+        controller.signUpWithEmail(
+            email: email,
+            password: password,
+            success: {
+                SVProgressHUD.dismiss()
+                ScreenTransitionManager.moveToMapBrowserViewController()
+        },
+            failure: { [weak self] in
+                SVProgressHUD.dismiss()
+                AlertManager.showConnectionErrorAlertCancellable(retryTask: { [weak self] in
+                    guard let sself = self else { return }
+                    sself.signUp()
+                })
+        })
+    }
+    
+    @IBAction func onSignInButtonTapped(_ sender: UIButton) {
+        signIn()
+    }
+    
+    @IBAction func onCreateButtonTapped(_ sender: UIButton) {
+        signUp()
+    }
+    
+//    @IBAction func onBackButtonTapped(_ sender: UIButton) {
+//        _ = navigationController?.popViewController(animated: true)
+//    }
 }
+
+
+
